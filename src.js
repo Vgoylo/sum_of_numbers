53c1a9aa02554a17a697947743ddf4af8979be12
Vgoylo <y_draiv.by@mail.ru>
2021-05-18 11:05:42 +0300

    Remove comments

--- src.js
+++ src.js
@@ -4,7 +4,6 @@ let namber = [1,2,3,4,5,6,7,8,9]
     for (let i = 0; i < numbers.length; i +=1){
         let nam = numbers[i];
         sum += nam
-       // if ( nam % 2 == 0);
     };
     return sum;
  }
