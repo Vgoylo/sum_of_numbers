1c643821f1b1402c1fadf94b2a52dfb94829c195
Vgoylo <y_draiv.by@mail.ru>
2021-05-21 22:55:04 +0300

    Check if number is greater zero

---
+++ number.js
@@ -0,0 +1,16 @@
+function isLessThanZero(number){
+    let result;
+    if(number > 0){
+        result = false;
+    }else{
+        result = true;
+    }
+    return result;
+} 
+
+
+function isLessThanZero(number){
+   return !(number > 0);
+    
+
+} 
