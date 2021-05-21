332e7cbf3877d885db17c470833e043e2db5057e
Vgoylo <y_draiv.by@mail.ru>
2021-05-21 21:45:12 +0300

    Middle Symbol

--- src.js
+++ src.js
@@ -1,13 +1,4 @@
-/*let namber = [1,2,3,4,5,6,7,8,9]
- function sum(numbers){
-    let sum = 0;
-    for (let i = 0; i < numbers.length; i +=1){
-        let nam = numbers[i];
-        sum += nam
-    };
-    return sum;
- }
-console.log(sum(namber))*/
+
 
 function middleSymbol(str){
     let result;
