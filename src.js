1ecfc6fefda08fc22674ef7b55970e5257678efe
Vgoylo <y_draiv.by@mail.ru>
2021-05-21 16:43:39 +0300

    Implement function calcylenting the middle simbyls

--- src.js
+++ src.js
@@ -1,4 +1,4 @@
-let namber = [1,2,3,4,5,6,7,8,9]
+/*let namber = [1,2,3,4,5,6,7,8,9]
  function sum(numbers){
     let sum = 0;
     for (let i = 0; i < numbers.length; i +=1){
@@ -7,4 +7,22 @@ let namber = [1,2,3,4,5,6,7,8,9]
     };
     return sum;
  }
-console.log(sum(namber))
+console.log(sum(namber))*/
+
+function middleSymbol(str){
+    let result;
+    let l = str.length;
+    
+    let whole = ~~(l/2);
+
+    let tract = l % 2;
+    if(tract == 0){
+        result = str[whole -1] + str[whole];
+    }else{
+        result = str[whole];
+    }
+    return result;
+    
+
+}
+
