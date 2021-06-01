a6c5f8f6d1d9e9a895edf341807651e495a951f0
Vgoylo <y_draiv.by@mail.ru>
2021-06-01 15:33:56 +0300

    Remove empty lines

--- leapYear.js
+++ leapYear.js
@@ -1,11 +1,11 @@
 function leapYear(year){
     let result;
+    
     if (year % 4 ==0){
         result = (year % 100 ==0) ? (year % 400 ==0) : true;
-
     } else {
         result = false;
-
     }
+
     return result;
 };
