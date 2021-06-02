19b51895b8701757ca71cb55e17a3bd5b251754d
Vgoylo <y_draiv.by@mail.ru>
2021-06-02 22:32:59 +0300

    NumbersSum

---
+++ numbers.js
@@ -0,0 +1,14 @@
+function numbersSum(number){
+
+    let result = 0;
+
+    
+    for (let i = 0; i < number+1 ; i+=1){
+       
+        result += i;
+        
+    }
+
+    return result;
+    
+}
