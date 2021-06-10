4e6d2070172c4c9ee58ba27f0f78804f3e14d5fc
Vgoylo <y_draiv.by@mail.ru>
2021-06-10 13:49:07 +0300

    Add spaseApart.js

---
+++ spaseApart.js
@@ -0,0 +1,28 @@
+function spaseApart(arr){
+    let result;
+    let sum = 0;
+    let flag = false;
+
+    arr.forEach(element => {
+        
+        if (element < 0){
+            console.log(element)
+            result = "invalid";
+            return;
+       
+        } else {
+            if(flag == true && element != '1'){
+                sum += element;
+
+            }
+            if(element === '1'){
+                flag = !false;
+            }
+            
+        }
+
+
+    });
+
+return result || sum;
+}
