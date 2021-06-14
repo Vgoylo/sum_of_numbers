bcabd9271674062250b652ef2e0ec1c0364d59d1
Vgoylo <y_draiv.by@mail.ru>
2021-06-14 19:50:04 +0300

    Replace for with foreach

--- new.js
+++ new.js
@@ -1,33 +1,14 @@
-// put your code here
-let array1 = [1, 2, 6, 7, 10];
- function err (array1){
-     for (let i = 0; i < array1.length; i+=1);
-}
- console.log()
+function  common (array1,array2){
+  let result = [];
+     
+  array2.forEach(element => {
+    if (array1.includes(element)){
+      result.push(element)
+    }
+    
+  });
+  
+    return result;
 
-
-let erray2 = [3, 6, 10, 15];
- function  common (array1,array2){
- let result = [];
-     for (let i = 0; i < array2.length; i+=1){
-     let c = array2[i]
-     if(array1.includes(c)){
-         result.push(c)
-        
-     }
-     return result;
-
- }
- }
- console.log()
-
-=> [6, 10]
-
-
-
-let erray = [1, 2, 6, 222, 7, 10]
-
-    let sortNumbers = erray.filter(num =>{
-        return num <= 9
-    })
-    console.log(sortNumbers)
+  }
+  
